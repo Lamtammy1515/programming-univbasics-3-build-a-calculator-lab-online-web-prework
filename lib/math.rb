06ebce23149569b961ddbc9d928516760cb86619
Tammy Lam <lamtammy1515@gmail.com>
2020-01-20 22:26:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,17 +11,25 @@ num1 - num2
 end
 
 def division(num1, num2)
-
+num1 = 10 
+num2 = 5 
+num1 - num2 
 end
 
 def multiplication(num1, num2)
-
+num1 = 10 
+num2 = 5 
+num1 - num2 
 end
 
 def modulo(num1, num2)
-
+num1 = 10 
+num2 = 5 
+num1 - num2 
 end
 
 def square_root(num)
-
+num1 = 10 
+num2 = 5 
+num1 - num2 
 end
